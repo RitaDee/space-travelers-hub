3eadeb4560fa1acc71756c23b68b89c388da730f
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-14 15:12:38 +0100

    Update store.js file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionsSlice, { missionsSlice } from './mission/MissionSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    mission: missionsSlice,
+  },
 });
 
 export default store;
