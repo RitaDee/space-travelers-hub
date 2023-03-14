b0bf59d063c3d425143e7e4775f234b4f5d6f759
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-14 15:28:26 +0100

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import React, { useEffect } from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
 import Missions from './routes/Missions';
 import Profile from './routes/Profile';
 import RocketLists from './routes/RocketLists';
 
+import { fetchMissions } from './redux/mission/MissionSlice';
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions);
+  }, [dispatch]);
+
   return (
     <div className="App">
 
