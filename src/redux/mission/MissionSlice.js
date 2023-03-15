6ceca905dac443f7d1a2f9f93a1249ac21f029b5
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-15 04:47:13 +0100

    Update MissionSlice.js file

--- src/redux/mission/MissionSlice.js
+++ src/redux/mission/MissionSlice.js
@@ -2,14 +2,13 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const url = 'https://api.spacexdata.com/v3/missions';
 
-export const fetchMissions = createAsyncThunk('missions/fetch',
-  async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  });
+export const fetchMissions = createAsyncThunk('missions/fetch', async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+});
 
-export const missionsSlice = createSlice({
+const missionsSlice = createSlice({
   name: 'missions',
   initialState: {
     list: [],
@@ -17,11 +16,12 @@ export const missionsSlice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchMissions.fulfilled, (state, action) => ({
-      ...state,
-      isFetching: false,
-      list: action.payload,
-    }))
+    builder
+      .addCase(fetchMissions.fulfilled, (state, action) => ({
+        ...state,
+        isFetching: false,
+        list: action.payload,
+      }))
       .addCase(fetchMissions.pending, (state) => ({
         ...state,
         isFetching: true,
@@ -30,4 +30,5 @@ export const missionsSlice = createSlice({
 });
 
 export default missionsSlice.reducer;
-export const selectMissions = (state) => state.missions.lists;
+
+export const selectMissions = (state) => state.missions.list;
