2e01a090129b87b06f9bddd60b9f3d80d3c6714e
Oscah01 <pipkillerinfo@gmail.com>
2023-03-16 10:51:45 +0300

    add rocket-profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { filterReservedMissions } from '../redux/mission/MissionSlice';
 import { filterReservedRockets } from '../redux/rockets/rocketsSlice';
 import styles from '../styles/Profile.module.css';
 
 const Profile = () => {
-  const reservedMissions = useSelector(filterReservedMissions);
   const reservedRockets = useSelector(filterReservedRockets);
 
   return (
     <main className={styles.main}>
-      <section className={styles.section}>
-        <h2 className={styles.title}>My Missions</h2>
-        <ul className={styles.list}>
-          {reservedMissions.map((e) => (
-            <li key={e.mission_id} className={styles.listItem}>
-              {e.mission_name}
-            </li>
-          ))}
-        </ul>
-      </section>
       <section className={styles.section}>
         <h2 className={styles.title}>My Rockets</h2>
         <ul className={styles.list}>
